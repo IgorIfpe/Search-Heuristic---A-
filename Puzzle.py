4c183be2f21468892e9ed8ec0172b54e6223d9ea
IGOR JOSE RIBEIRO DOS SANTOS <ijrs1@discente.ifpe.edu.br>
2022-09-27 10:17:22 -0300

    Calculate H

--- Puzzle.py
+++ Puzzle.py
@@ -1,24 +1,37 @@
 class Puzzle:
 
     def __init__(self, data: list, expected_state: list) -> None:
-        self.g = 0
+        self.g = 1
         self.h = 0
         self.f = 0
         self.data = data
         self.expected_state = expected_state
+        self.possibilities = []
+        self.calculate()
 
-    def calculateH(self):
+    def calculateH(self) -> int:
+        h = 0
         if len(self.data) == len(self.expected_state):
             for x, line in enumerate(self.expected_state):
                 for y in line:
-                    expected_position = self.findSpace(y, True)
-                    current_position = self.findSpace(y)
+                    if y != 0:
+                        expected_position = self.findSpace(y, True)
+                        current_position = self.findSpace(y)
 
-                    print(f"Y: {y} Expected: {expected_position} Current: {current_position}\n")
+                        x_sum = abs(expected_position[0] - current_position[0])
+                        y_sum = abs(expected_position[1] - current_position[1])
+                        h += x_sum + y_sum
+
+                        print(f"Y: {y} Expected: {expected_position} Current: {current_position} H: {x_sum + y_sum}\n")
+        return h
+
+    def calculate(self):
+        self.h = self.calculateH()
+        self.g = 1
+        self.f = self.h + self.g
 
     def findSpace(self, spaceFinded, isFinalState: bool = False) -> tuple:
         matriz = self.data if not isFinalState else self.expected_state
-        print(matriz)
         for x, line in enumerate(matriz):
             if spaceFinded in line:
                 return (x, line.index(spaceFinded))
@@ -37,4 +50,5 @@ matriz_b = [
 ]
 
 p = Puzzle(matriz_b, matriz_a)
-p.calculateH()
+p.calculate()
+print(p.h)
