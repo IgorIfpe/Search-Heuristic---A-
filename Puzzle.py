f141f077c22dd15f4f1a81735274a401236a3665
IGOR JOSE RIBEIRO DOS SANTOS <20192ewbj0124@belojardim.ifpe>
2022-09-28 09:14:30 -0300

    Get mins H

--- Puzzle.py
+++ Puzzle.py
@@ -1,7 +1,9 @@
 from copy import deepcopy
-
+import operator
 
 class Puzzle:
+
+    current_min_h = None
 
     def __init__(self, data: list, expected_state: list, last_state: list = []) -> None:
         self.g = 1
@@ -11,16 +13,16 @@ class Puzzle:
         self.expected_state = expected_state
         self.last_state = last_state
         self.possibilities = []
-        self.calculate()
+        if self.data != self.expected_state: self.calculate()
 
-    def calculateH(self) -> int:
+    def calculateH(self, state: list) -> int:
         h = 0
         if len(self.data) == len(self.expected_state):
-            for x, line in enumerate(self.expected_state):
+            for x, line in enumerate(state):
                 for y in line:
                     if y != 0:
-                        expected_position = self.findSpace(y, True)
-                        current_position = self.findSpace(y)
+                        expected_position = self.findSpace(y, self.expected_state)
+                        current_position = self.findSpace(y, state)
 
                         x_sum = abs(expected_position[0] - current_position[0])
                         y_sum = abs(expected_position[1] - current_position[1])
@@ -30,20 +32,19 @@ class Puzzle:
         return h
 
     def calculate(self):
-        self.h = self.calculateH()
+        self.h = self.calculateH(self.expected_state)
         self.g = 1
         self.f = self.h + self.g
         self.measurePossibilities()
 
-    def findSpace(self, spaceFinded, isFinalState: bool = False) -> tuple:
-        matriz = self.data if not isFinalState else self.expected_state
-        for x, line in enumerate(matriz):
+    def findSpace(self, spaceFinded, state: list) -> tuple:
+        for x, line in enumerate(state):
             if spaceFinded in line:
                 return (x, line.index(spaceFinded))
         return ()
-
+        
     def measurePossibilities(self) -> None:
-        zero_position = self.findSpace(0)
+        zero_position = self.findSpace(0, self.data)
         x_ = zero_position[0]
         y_ = zero_position[1]
 
@@ -54,13 +55,35 @@ class Puzzle:
         if (y_+1 < max_): self.addPossibilites(self.swapPosition((x_, y_+1)))
         if (y_-1 > -1): self.addPossibilites(self.swapPosition((x_, y_-1)))
 
+        self.transformPosibilitesInPuzzles()
+
+    def transformPosibilitesInPuzzles(self):
+        sums = []
+        for pos, p in enumerate(self.possibilities):
+            sums.append(
+                (pos, self.calculateH(p[0]))
+            )
+        print(sums)
+        tuple_min = min(sums, key=operator.itemgetter(1))
+        mins = [i if (i[1] == tuple_min[1]) else None for i in sums]
+        mins.remove(None)
+        print(mins)
+        for i in mins:
+            if Puzzle.current_min_h == None:
+                Puzzle.current_min_h = i[1]
+            elif i[1] <= Puzzle.current_min_h:
+                Puzzle.current_min_h = i[1]
+                element = self.possibilities[i[0]]
+                #Puzzle(element[0], element[1], element[2])
+        #self.possibilities = [Puzzle() for p in self.possibilities]
+
     def addPossibilites(self, possibiliti):
         if (possibiliti != []):
             self.possibilities.append(possibiliti)
 
     def swapPosition(self, new_position):
         result = deepcopy(self.data)
-        zero_position = self.findSpace(0)
+        zero_position = self.findSpace(0, result)
         x_ = zero_position[0]
         y_ = zero_position[1]
 
